f40d53072a5a218b51ba131499243389bb9608da
Srude-06 <vibhuoffice1002@gmail.com>
2024-10-14 10:58:49 +0530

    Changes 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
-// import logo from './logo.svg';
-// import './App.css';
+// // import logo from './logo.svg';
+// // import './App.css';
+// import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+// import Page1 from './Page1'; 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <p>
+//           changes made in react app
+//         </p>
+//         <a
+//           href="https://reactjs.org"
+//         >
+//           Welcome to React-Aws  
+//         </a>
+//         <nav>
+//             <Link to="/Page1">Go to Page 1</Link>
+//           </nav>
+
+//         <Routes>
+//           <Route path="/Page1" element={<Page1 />} /> {/* Route for Page1 */}
+//         </Routes>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// import './App.css'; // Uncomment if you have a CSS file to import
+import { Route, Link, Routes } from 'react-router-dom';
+import Page1 from './Page1'; 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          changes made in react app
+          Changes made in the React app
         </p>
-        <a
-          href="https://reactjs.org"
-        >
-          Welcome to React-Aws  
-        </a>
+        <nav>
+          <Link to="/page1">Go to Page 1</Link> 
+        </nav>
       </header>
+
+      
+      <Routes>
+        <Route path="/page1" element={<Page1 />} /> 
+      </Routes>
     </div>
   );
 }
---
+++ src/Page1.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Page1 = () => {
+  return (
+    <div>
+      <h1>Welcome to Page 1</h1>
+      <p>This is a simple page with basic content.</p>
+    </div>
+  );
+};
+
+export default Page1;
