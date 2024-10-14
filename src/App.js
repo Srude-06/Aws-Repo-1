9b5cd8e2adf1f708e19bf2e298dbdabe2b6b0a91
Srude-06 <vibhuoffice1002@gmail.com>
2024-10-14 14:34:45 +0530

    changes added to branch

--- src/App.js
+++ src/App.js
@@ -1,56 +1,44 @@
-// // import logo from './logo.svg';
-// // import './App.css';
-// import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
-// import Page1 from './Page1'; 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <p>
-//           changes made in react app
-//         </p>
-//         <a
-//           href="https://reactjs.org"
-//         >
-//           Welcome to React-Aws  
-//         </a>
-//         <nav>
-//             <Link to="/Page1">Go to Page 1</Link>
-//           </nav>
 
-//         <Routes>
-//           <Route path="/Page1" element={<Page1 />} /> {/* Route for Page1 */}
-//         </Routes>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-// import './App.css'; // Uncomment if you have a CSS file to import
-import { Route, Link, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 import Page1 from './Page1'; 
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Changes made in the React app.This is my app/home page 
-        </p>
-        <nav>
-          <Link to="/page1">Go to Page 1</Link> 
-        </nav>
-      </header>
+    <Router> {/* Wrap everything inside Router */}
+      <div className="App">
+        <header className="App-header">
+          <p>
+            Changes made in the React app
+          </p>
+          <a href="https://reactjs.org">
+            Welcome to React-Aws  
+          </a>
+          <nav>
+            <Link to="/page1">Go to Page 1</Link> {/* Ensure the path matches the route */}
+          </nav>
 
-      
-      <Routes>
-        <Route path="/page1" element={<Page1 />} /> 
-      </Routes>
-    </div>
+          <Routes>
+            <Route path="/page1" element={<Page1 />} /> {/* Use lowercase 'page1' in the path */}
+          </Routes>
+        </header>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
